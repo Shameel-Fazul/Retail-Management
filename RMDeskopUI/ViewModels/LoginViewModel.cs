ec645b1274ea5faac0415da1c18592066097ea57
Shameel Fazul <shameel.fazul@mail.com>
2021-08-03 18:35:02 +0530

    Implemented error handlers to the login system

--- RMDeskopUI/ViewModels/LoginViewModel.cs
+++ RMDeskopUI/ViewModels/LoginViewModel.cs
@@ -41,6 +41,35 @@ namespace RMDeskopUI.ViewModels
             }
         }
 
+
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
         public bool CanLogIn
         {
             get 
@@ -60,11 +89,12 @@ namespace RMDeskopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception err)
             {
-                Console.WriteLine(err.Message);
+                ErrorMessage = err.Message;
             }
         }
 
