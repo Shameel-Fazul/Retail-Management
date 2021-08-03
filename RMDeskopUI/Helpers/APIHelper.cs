803a140c0a8f29a1d421ec9b737f4f61d11ff7d6
Shameel Fazul <shameel.fazul@mail.com>
2021-08-03 16:43:57 +0530

    Wired up desktop with the backend and implemented authentication

---
+++ RMDeskopUI/Helpers/APIHelper.cs
@@ -0,0 +1,55 @@
+﻿using RMDeskopUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDeskopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+        private HttpClient apiClient { get; set; }
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+            apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri(api);
+            apiClient.DefaultRequestHeaders.Accept.Clear(); // clears default header
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); // accepts json only
+        }
+
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            var date = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            using (HttpResponseMessage response = await apiClient.PostAsync("/Token", date))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+
+            }
+        }
+    }
+}
---
+++ RMDeskopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using RMDeskopUI.Models;
+using System.Threading.Tasks;
+
+namespace RMDeskopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
