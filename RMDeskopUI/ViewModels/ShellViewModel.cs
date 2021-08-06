4e3c81957a0cc67723c7aea398c2b37bcf3a5514
Shameel Fazul <shameel.fazul@mail.com>
2021-08-06 17:15:20 +0530

    Implemented Event Aggregation. Shell view now activates the sales view model after authentication

--- RMDeskopUI/ViewModels/ShellViewModel.cs
+++ RMDeskopUI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using RMDeskopUI.EventModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +8,29 @@ using System.Threading.Tasks;
 
 namespace RMDeskopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private LoginViewModel _loginVM;
-        public ShellViewModel(LoginViewModel loginVM)
+        private IEventAggregator _events;
+        private SalesViewModel _salesVM;
+        private SimpleContainer _container;
+
+        public ShellViewModel(LoginViewModel loginVM, IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
         {
+            _events = events;
             _loginVM = loginVM;
+            _salesVM = salesVM;
+            _container = container;
+
+            _events.Subscribe(this);
+            
             ActivateItem(_loginVM);
+        }
+
+        public void Handle(LogOnEvent message)
+        {
+            ActivateItem(_salesVM);
+            _loginVM = _container.GetInstance<LoginViewModel>(); // overwrite current loginvm instance with new instance to wipe all existing data
         }
     }
 }
