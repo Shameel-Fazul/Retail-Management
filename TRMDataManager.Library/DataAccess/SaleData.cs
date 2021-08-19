22fc0cac128c53c416aeeb86ba5d801af314e1d3
Shameel Fazul <shameel.fazul@mail.com>
2021-08-19 16:05:19 +0530

    API - Getting sales report - Getting inventory - Insert products into inventory

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -81,21 +81,16 @@ namespace TRMDataManager.Library.DataAccess
                     sql.RollbackTransaction();
                     throw;
                 }
-            }
-
-            
-
-            
+            }            
         }
 
+        public List<SaleReportModel> GetSaleReport()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
 
-        //public List<ProductModel> GetProducts()
-        //{
-        //    SqlDataAccess sql = new SqlDataAccess();
+            var output = sql.LoadData<SaleReportModel, dynamic>("dbo.spSale_SaleReport", new { }, "TRMData");
 
-        //    var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "TRMData");
-
-        //    return output;
-        //}
+            return output;
+        }
     }
 }
