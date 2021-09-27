6c38c69b73f1894c7f40dc21d4c4e9a9feb4791d
Shameel Fazul <shameel.fazul@mail.com>
2021-09-27 17:25:39 +0530

    Implemented the registration system in the Blazor WebAssembly app

---
+++ TRMDesktopUI.Library/Models/CreateUserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TRMDesktopUI.Library.Models
+{
+    public class CreateUserModel
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Confirm Password")]
+        [Compare(nameof(Password), ErrorMessage = "The passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
