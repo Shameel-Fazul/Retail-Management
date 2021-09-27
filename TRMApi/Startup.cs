9d4bc5fdd1ce36db0254a9da723341107aa4a75f
Shameel Fazul <shameel.fazul@mail.com>
2021-09-27 17:52:12 +0530

    Warning fixes 2 - database developer exception for .NET 6 deprecation changes

--- TRMApi/Startup.cs
+++ TRMApi/Startup.cs
@@ -31,6 +31,7 @@ namespace TRMApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddCors(policy =>
             {
                 policy.AddPolicy("OpenCorsPolicy", opt => opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
@@ -85,7 +86,7 @@ namespace TRMApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
