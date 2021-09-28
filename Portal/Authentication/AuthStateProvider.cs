621dceb3517cf51b37ca9f0f8804feec2c2e6078
Shameel Fazul <shameel.fazul@mail.com>
2021-09-28 19:13:22 +0530

    WebAssembly app - Login system
    JWT initial verification
    Accessing authorized routes from the UI class library

--- Portal/Authentication/AuthStateProvider.cs
+++ Portal/Authentication/AuthStateProvider.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using TRMDesktopUI.Library.Api;
+using TRMDesktopUI.Library.Models;
 
 namespace Portal.Authentication
 {
@@ -16,13 +18,17 @@ namespace Portal.Authentication
         private readonly HttpClient _httpClient;
         private readonly ILocalStorageService _localStorage;
         private readonly IConfiguration _config;
+        private readonly IAPIHelper _apiHelper;
+        private readonly ILoggedInUserModel _loggedInUser;
         private readonly AuthenticationState _anonymous; 
 
-        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage, IConfiguration config)
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage, IConfiguration config, IAPIHelper apiHelper, ILoggedInUserModel loggedInUser)
         {
             _httpClient = httpClient;
             _localStorage = localStorage;
             _config = config;
+            _apiHelper = apiHelper;
+            _loggedInUser = loggedInUser;
             _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
         }
 
@@ -36,21 +42,53 @@ namespace Portal.Authentication
                 return _anonymous;
             }
 
+            bool isAuthenticated = await NotifyUserAuthentication(token);
+
+            if (isAuthenticated == false)
+            {
+                return _anonymous;
+            }
+
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
 
             return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
         }
 
-        public void NotifyUserAuthentication(string token)
+        public async Task<bool> NotifyUserAuthentication(string token)
         {
-            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType"));
-            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
-            NotifyAuthenticationStateChanged(authState);
+            Task<AuthenticationState> authState;
+            bool isAutheticatedCheck;
+
+            try
+            {
+                await _apiHelper.GetLoggedInUserInfo(token);
+                Console.WriteLine($"{_loggedInUser.FirstName} just logged in!");
+                var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType"));
+                authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+                NotifyAuthenticationStateChanged(authState);
+                isAutheticatedCheck = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await NotifyUserLogout();
+                isAutheticatedCheck = false;
+            }
+
+            return isAutheticatedCheck;
         }
 
-        public void NotifyUserLogout()
+        public async Task NotifyUserLogout()
         {
             var authState = Task.FromResult(_anonymous);
+
+            string authTokenStorageKey = _config["authTokenStorageKey"];
+            await _localStorage.RemoveItemAsync(authTokenStorageKey);
+            
+            _loggedInUser.ResetUserModel();
+            _apiHelper.LogOffUser();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+
             NotifyAuthenticationStateChanged(authState);
         }
     }
